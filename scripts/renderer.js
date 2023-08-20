0626cc4e25e5ae592ba43a60cc2311fbd635a6e5
Tuhin Subhra De <tuhinsubhrade069@gmail.com>
2023-08-20 23:30:08 +0530

    Added Comments

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,8 +1,13 @@
 //Begin
 var cvwindow;
 
+//Creating a new window for rendering the CV
 cvwindow = window.open("", "_blank", "height=800,width=601");
+
+//parent element of the whole ERP CV form (Oogway was the boss in Kung Fu Panda)
 var oogway = document.getElementById("myframe").contentDocument.documentElement; //parent
+
+//Preventing to render CV on Company list page, both CV making form and this have same URL
 try {
     var pref1_parent = oogway.querySelector("#cv1_pref1").parentElement;
 } catch {
@@ -10,11 +15,11 @@ try {
     throw new Error("Something went wrong");
 }
 
-var student_name = oogway.querySelector("#full_name").value;
+var student_name = oogway.querySelector("#full_name").value; //Student's name
 
 // -----------------------------------------//---------------------------------------//
 
-//Populating cv1 preference order, initially
+//Populating cv1 preference order (Internships, Projects etc...)
 var cv1_pref = []  // Stores preference order of elements for cv1
 
 function populate_pref1() {
@@ -25,16 +30,18 @@ function populate_pref1() {
 }
 populate_pref1();
 
+//The Terms in order dropdown
 var order_fields = ["Internships", "Projects", "Internships and Projects", "Academic Achievement", "Certification",
     "Training", "Experience", "Entrepreneurial Experience", "Competition/Conference",
     "Publication", "Position of Responsibilities", "Extra-Curricular Activities",
     "Skills and Expertise", "Coursework Information"];
 
+//The terms in element dropdown 
 var element_fields = ["Internship", "Project", "Internship/Project", "Academic Achievement", "Certification",
     "Training", "Experience", "Entrepreneurial Experience", "Competition/Conference",
     "Publication", "Position of Responsibilities"];
 
-
+//function to fill Skills, EAA and Coursework info
 function fill_xtras() {
     var course_info = oogway.querySelector('[title="Rich Text Editor, research_area"]').contentDocument.documentElement;
     course_info = course_info.querySelector("body").innerHTML;
@@ -51,7 +58,8 @@ function fill_xtras() {
     return xtras;
 }
 
-
+//The structure which is displayed in the CV (Title and its description)
+// This returns the title and description mapped
 function fill_assigner() {
     var assigner = new Map();
     for (let i = 0; i < element_fields.length; i++)
@@ -77,6 +85,8 @@ function fill_assigner() {
     return assigner;
 }
 
+//Displaying the content in CV1 by returning the HTML tags concatenated
+//element_field maybe sth like Internships, Projects. Aggregate all of them together
 function display_content_cv1(element_field) {
     var assigner = fill_assigner();
     var size = assigner.get(element_field).length;
@@ -90,10 +100,14 @@ function display_content_cv1(element_field) {
     return return_object;
 };
 
+//Displaying CV1 by order as required
 function display_cv1_byorder() {
     var return_object = ``;
-    var xtras = fill_xtras();
+    var xtras = fill_xtras(); //fill the xtras
+
+    //collecting all elements as ordered in CV1 pref list
     for (let i = 0; i < cv1_pref.length; i++) {
+        //oem stands for Original Element Name like Internships, Projects
         var oem = cv1_pref[i];
         var idx_in_order_fields = order_fields.indexOf(oem);
         return_object += `<p class="section-header">${oem.toUpperCase()}</p>`;
@@ -108,6 +122,9 @@ function display_cv1_byorder() {
     return return_object;
 }
 
+
+//rendering the whole CV in the new generated window
+//This function runs every time something is changed in the CV
 function render_cv() {
     //mywindow.blur();
     var writing_element = `<head>
@@ -139,7 +156,8 @@ function render_cv() {
 
 render_cv();
 
-//--------------- For Update button only when CV editing/making is active in ERP ----------------//
+//--------------- For Update button only when CV portal is active in ERP ----------------//
+// The window closes after clicking update button on ERP Portal
 var update_button = oogway.querySelector("#saveprdata")
 if (update_button != null) {
     const cl_message = "You have updated your CV in ERP. This window will close, Generate CV again";
@@ -157,11 +175,16 @@ var cw_info = oogway.querySelector('[title="Rich Text Editor, research_area"]').
 var sknexp = oogway.querySelector('[title="Rich Text Editor, skill"]').contentDocument.documentElement.querySelector("body");
 var eaa = oogway.querySelector('[title="Rich Text Editor, eaa"]').contentDocument.documentElement.querySelector("body");
 
-evc_text(cw_info); evc_text(sknexp); evc_text(eaa);
+//Event listeners for xtras
+evc_text(cw_info);
+evc_text(sknexp); 
+evc_text(eaa);
 
+//Selecting the big table which contains the elements descriptions
 var element_table = oogway.querySelector("#profTab");
 var element_table_child = element_table.querySelectorAll("#profTr");
 
+//adding event listeners to individual elements in the table
 for (let i = 7; i <= 56; i++){
     var category = oogway.querySelector("#standard" + i);
     var cat_head = oogway.querySelector("#university" + i);
@@ -174,7 +197,7 @@ for (let i = 7; i <= 56; i++){
     evc_text(etc_des.contentDocument.documentElement.getElementsByTagName("body")[0]);
 }
 
-// Preference order of cv1
+// Event listener for Preference order of cv1
 pref1_parent.addEventListener('change', () => { populate_pref1(); render_cv(); }, false);
 
 /*/------------------------------- End ------------------------------------------------/*/
